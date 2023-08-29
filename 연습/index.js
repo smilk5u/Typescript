6ea5c6bcfa3a8f589aa9960c56c2510a3afb0487
amilk5u <amilk5u@naver.com>
2023-08-30 00:35:59 +0900

    타입스크립트 공부

---
+++ 연습/index.js
@@ -0,0 +1,8 @@
+// number
+// string
+// boolean
+// null
+// undefined
+// any
+var type = 2;
+console.log(type);
---
+++ 연습/index.ts
@@ -0,0 +1,12 @@
+// number
+// string
+// boolean
+// null
+// undefined
+// any
+
+
+
+let type:number = 2;
+
+console.log(type);
